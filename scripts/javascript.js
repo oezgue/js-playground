84d8ac7952dd63c3016d18f1f871ce4d174fb489
oezgue <oezgue@gmail.com>
2025-01-12 16:04:09 +0100

    Loop through an array with .map

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,1 +1,10 @@
 console.log('Hello, oezgue!');
+const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+function logPlanets() {
+  console.log('Here is a list of the planets:');
+
+  planets.map( (planet) => {console.log('Hello ' + planet)});
+}
+
+logPlanets();
